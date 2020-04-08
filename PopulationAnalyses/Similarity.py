aea44cac509b16ae36f5f8360dc4c8bc6a61be62
Will Mau <wmau@bu.edu>
2020-04-08 19:27:59 -0400

    Added code for analyzing similarity and transient rate changes

--- PopulationAnalyses/Similarity.py
+++ PopulationAnalyses/Similarity.py
@@ -1,30 +1,48 @@
 from CaImaging.CellReg import *
 import numpy as np
-from SEFL_utils import project_dirs
+from SEFL_utils import project_dirs, load_cellmap, batch_load
 from CaImaging.util import bin_transients, filter_sessions
-
+import matplotlib.pyplot as plt
 
 def session_similarity(sessions, bin_size_in_seconds=30):
 
     # Get cell registration.
-    cellreg_path = get_cellreg_path(sessions, sessions.loc[0, 'Mouse'])
-    C = CellRegObj(cellreg_path)
-    cell_map = trim_map(C.map, list(sessions.Session))
+    cell_map = load_cellmap(sessions, detected='either_day')
 
-    session_paths = sessions.Path
-    data = []
-    for path, session_column in zip(session_paths, list(cell_map)):
-        minian = open_minian(path)
-        S = np.asarray(minian.S)
-        cells = cell_map[session_column]
-        data.append(bin_transients(S[cells], bin_size_in_seconds))
+    # Load each session.
+    data = batch_load(sessions, 'S')
+
+    # Rearrange neurons based on registration mappings.
+    # Then bin the transients.
+    data = rearrange_neurons(cell_map, data)
+    transients = []
+    for session in data:
+        transients.append(bin_transients(session, bin_size_in_seconds))
+
+    # Do every correlation between time bins.
+    r = np.corrcoef(np.hstack(transients).T)
+    plt.imshow(r, cmap='bwr')
+    plt.clim([-1, 1])
+    plt.show()
+    pass
+
+
+def transient_rate_change(sessions, bin_size_in_seconds=30):
+    cell_map = load_cellmap(sessions, detected='everyday')
+    data = batch_load(sessions, 'S')
+
+    data = rearrange_neurons(cell_map, data)
+    transients = []
+    for session in data:
+        transients.append(np.mean(bin_transients(session, bin_size_in_seconds), axis=1))
+    plt.plot(np.vstack(transients), '.-')
 
     pass
 
 if __name__ == '__main__':
     df, path = project_dirs()
     keys = ['Mouse', 'Session']
-    keywords = ['pp1', 'Baseline', 'TraumaPost']
+    keywords = ['pp2', 'TraumaTest', 'MildStressor', 'SEFLtest']
     sessions = filter_sessions(df, keys, keywords, 'all')
 
     # minian_outputs = []
@@ -38,5 +56,7 @@ if __name__ == '__main__':
     # template = rearrange_neurons(cell_map[:,0], [template])
     #
     # lapsed_activation(template[0], lapsed)
-    session_similarity(sessions)
+
+    #session_similarity(sessions)
+    transient_rate_change(sessions)
     pass
--- SEFL_utils.py
+++ SEFL_utils.py
@@ -1,10 +1,31 @@
 import os
 import pandas as pd
+from CaImaging.CellReg import *
 
 project_path = r'Z:\Will\SEFL'
 directories_path = os.path.join(project_path, 'ProjectDirectories.csv')
 
 def project_dirs(path=directories_path):
     df = pd.read_csv(path)
+    df.Session = df.Session.str.lower()
+    df.Group = df.Group.str.lower()
 
     return df, path
+
+
+def load_cellmap(sessions, detected='either_day'):
+    cellreg_path = get_cellreg_path(sessions, sessions.Mouse.iloc[0])
+    C = CellRegObj(cellreg_path)
+    cell_map = trim_map(C.map, list(sessions.Session), detected=detected)
+
+    return cell_map
+
+
+def batch_load(sessions, datatype):
+    session_paths = sessions.Path
+    data = []
+    for path in session_paths:
+        minian = open_minian(path)
+        data.append(np.asarray(minian[datatype]))
+
+    return data
