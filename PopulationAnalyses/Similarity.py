1e2f07cb2de0d23b65601aa5ba3f67d507f8d6fb
Will Mau <wmau@bu.edu>
2020-05-13 15:28:45 -0400

    Some VSCode additions

--- PopulationAnalyses/Similarity.py
+++ PopulationAnalyses/Similarity.py
@@ -1,10 +1,27 @@
-from CaImaging.CellReg import *
-import numpy as np
-from SEFL_utils import project_dirs, load_cellmap, batch_load
-from CaImaging.util import bin_transients, filter_sessions
 import matplotlib.pyplot as plt
+import numpy as np
+import cv2 
 
-def session_similarity(sessions, bin_size_in_seconds=30):
+from CaImaging.CellReg import *
+from CaImaging.util import bin_transients, filter_sessions
+from SEFL_utils import batch_load, load_cellmap, project_dirs
+
+
+def session_similarity(sessions, bin_size_in_seconds=30, ax=None):
+    """
+    Compute a correlation matrix between 2+ sessions based on population
+    vectors made up of # of transients per bin.
+
+    :parameters
+    ---
+    sessions: pandas Series
+        List of sessions to compare.
+
+    bin_size_in_seconds: int
+        Number of seconds per bin.
+
+    return
+    """
 
     # Get cell registration.
     cell_map = load_cellmap(sessions, detected='either_day')
@@ -21,28 +38,43 @@ def session_similarity(sessions, bin_size_in_seconds=30):
 
     # Do every correlation between time bins.
     r = np.corrcoef(np.hstack(transients).T)
-    plt.imshow(r, cmap='bwr')
-    plt.clim([-1, 1])
-    plt.show()
-    pass
+
+    if ax is None:
+        fig, ax = plt.subplots()
+    ax.imshow(r, cmap='bwr')
+    ax.clim([-1, 1])
+
+    return r
 
 
-def transient_rate_change(sessions, bin_size_in_seconds=30):
+def transient_rate_change(sessions, bin_size_in_seconds=30, ax=None):
+    # Get cell registration.
     cell_map = load_cellmap(sessions, detected='everyday')
+
+    # Load each session.
     data = batch_load(sessions, 'S')
 
+    # Rearrange neurons based on registration mappings.
+    # Then bin the transients and take the mean across time.
     data = rearrange_neurons(cell_map, data)
     transients = []
     for session in data:
         transients.append(np.mean(bin_transients(session, bin_size_in_seconds), axis=1))
-    plt.plot(np.vstack(transients), '.-')
+
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    ax.plot(np.vstack(transients), 'k.-')
+    ax.set_xticks(range(len(sessions)))
+    ax.set_xticklabels(sessions.Session)
+    ax.set_ylabel('Average number of transients')
 
     pass
 
 if __name__ == '__main__':
     df, path = project_dirs()
     keys = ['Mouse', 'Session']
-    keywords = ['pp2', 'TraumaTest', 'MildStressor', 'SEFLtest']
+    keywords = ['pp1', 'Baseline', 'TraumaTest']
     sessions = filter_sessions(df, keys, keywords, 'all')
 
     # minian_outputs = []
